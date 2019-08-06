38807dc74ea9eab041a161a6a5591c37b1adb308
FritsvanLatum <f.a.vanlatum@vlfg.nl>
2019-08-06 17:06:06 +0200

    discovery service and KB service added

--- keys_worldcat_kb.php
+++
@@ -1,9 +0,0 @@
-<?php
-//circulation config
-
-$config = [];
-
-//wskey en secret van PPL-APIkey
-$config['wskey'] = "tA2yibd4vmeqTt7TYEmYNOzLHUWEGGF07v3dlR8YnvQni9wI0DLpBbARZtOZXpDsCFELxwV202smYFx8";
-
-?>
