955f9bd51d49cc3b3d33451e3f6348c929a5f6cb
FritsvanLatum <f.a.vanlatum@vlfg.nl>
2019-08-06 13:08:45 +0200

    availability service improved

--- Availability_Service.php
+++ Availability_Service.php
@@ -13,12 +13,13 @@ class Availability_Service extends OCLC_PPL_Service {
 
   private $avail_method = 'GET';
   private $avail_params = ['x-registryId' => '',
-                           'query' => '',
-                           'x-return-group-availability' => 0];
+  'query' => '',
+  'x-return-group-availability' => 0];
   private $avail_headers = ['Accept' => 'text/xml'];
 
-  public $avail = null;
-  
+  public $avail_xml = null;
+  public $avail_simpleXml = null;
+
   public function __construct($key_file) {
     parent::__construct($key_file);
     $this->avail_params['x-registryId'] = $this->institution;
@@ -32,16 +33,17 @@ class Availability_Service extends OCLC_PPL_Service {
     $json['avail_url'] = $this->avail_url;
     $json['avail_headers'] = $this->avail_headers;
     $json['avail_params'] = $this->avail_params;
-    $json['avail'] = $this->avail;
+    $json['avail_simpleXml'] = $this->avail_simpleXml;
+    //$json['avail_xml'] = $this->avail_xml;
     $json['ns'] = $this->ns;
 
     return json_encode($json, JSON_PRETTY_PRINT);
   }
 
   public function get_availabilty_of_ocn($ocn) {
-    return $this->get_availabilty_query('no:ocm'.$ocn); 
+    return $this->get_availabilty_query('no:ocm'.$ocn);
   }
-   
+
   public function get_availabilty_query($query) {
     //$query has to be a CQL query
     $this->avail_params['query'] = $query;
@@ -60,7 +62,7 @@ class Availability_Service extends OCLC_PPL_Service {
 
     curl_setopt($curl, CURLOPT_URL, $av_url);
     curl_setopt($curl, CURLOPT_HTTPHEADER, $header_array);
-    
+
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
     $result = curl_exec($curl);
@@ -86,31 +88,60 @@ class Availability_Service extends OCLC_PPL_Service {
         if ($error_number) {
           $this->log_entry('Error','get_avail',"Result but still cUrl error [$error_number]: $error_msg");
         }
-        
+
         $result = str_replace(array("\n", "\r", "\t"), '', $result);
         $result = trim(str_replace('"', "'", $result));
-
+        $this->avail_xml = $result;
         $simpleXml = new SimpleXMLElement($result);
         $this->ns = $simpleXml->getDocNamespaces(TRUE,TRUE);
         foreach ($this->ns as $prefix => $namespace) {
           if (strlen($prefix) == 0) $prefix = 'x';
           $simpleXml->registerXPathNamespace($prefix,$namespace);
         }
-        $this->avail = $simpleXml;
+        $this->avail_simpleXml = $simpleXml;
         return TRUE;
       }
     }
   }
-  
-  public function get_element_value($ocn,$element) {
-    if (($this->ocn != $ocn) || empty($this->avail)) $this->get_avail($ocn);
-    $matches = $this->avail->xpath('//'.$element);
+
+  private function get_element_value($element) {
     $result = array();
-    foreach ($matches as $match) {
-      $result[] = $match->__toString();
+    if (empty($this->avail_simpleXml)) {
+      //
+    }
+    else {
+      $result = $this->avail_simpleXml->xpath('//'.$element);
     }
     return $result;
   }
-  
+
+  public function get_circulation_info() {
+    //$circulations = $this->get_element_value('holdings/holding/circulations');
+    $holdings = $this->get_element_value('holdings/holding');
+    $result = array();
+    if (count($holdings) > 0) {
+      foreach ($holdings as $holding) {
+        if (!empty($holding)) {
+          $holdingdata = array();
+          $holdingdata['typeOfRecord'] = $holding->xpath('//typeOfRecord')[0]->__toString();
+          $holdingdata['shelvingLocation'] = $holding->xpath('//shelvingLocation')[0]->__toString();
+          $holdingdata['callNumber'] = $holding->xpath('//callNumber')[0]->__toString();
+          $circulations = $holding->xpath('circulations');
+          foreach ($circulations as $circ) {
+            if (!empty($circ)) {
+              $circdata = array();
+              $circdata['itemId'] = $circ->xpath('//itemId')[0]->__toString();
+              $circdata['availableNow'] = $circ->xpath('//availableNow/@value')[0]->__toString();
+              $circdata['renewable'] = $circ->xpath('//renewable/@value')[0]->__toString();
+              $circdata['onHold'] = $circ->xpath('//onHold/@value')[0]->__toString();
+              $holdingdata['circulation'][] = $circdata;
+            }
+          }
+        }
+        $result[]=$holdingdata;
+      }
+    }
+    return $result;
+  }
 }
 
--- test_Availability_Service.php
+++ test_Availability_Service.php
@@ -8,18 +8,28 @@
   <body>
     <?php
     require_once './Availability_Service.php';
-
     $avail = new Availability_Service('keys_availability.php');
     $avail->get_availabilty_of_ocn('1024083487');
-    ?>  
-    
+    //$avail->get_availabilty_of_ocn('402543568');
+    //$avail->get_availabilty_of_ocn('887933119');
+    //$avail->get_availabilty_of_ocn('1019254132');
+    ?>
+
     <p>Result of availability request:
       <pre>
-<?php echo $avail;?>
+        <?php 
+          echo $avail;
+
+        ?>
       </pre>
     </p>
 
-    
+    <p>Result of circulations:
+      <pre>
+        <?php echo json_encode($avail->get_circulation_info(),JSON_PRETTY_PRINT);?>
+      </pre>
+    </p>
+
   </body>
 
 </html>
